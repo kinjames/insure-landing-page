14afe2ce5a3b9cf0ad4783fc64c0e0333b92e1d3
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-08-22 23:05:45 +0000

    js final commit

--- apps/js/script.js
+++ apps/js/script.js
@@ -1,1 +1,17 @@
+const menuBtns = document.getElementById("menuBtn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const body = document.querySelector("body");
+
+menuBtns.addEventListener('click', ()=>{
+    if (mobileMenu.classList.contains('hide')){
+        menuBtns.classList.add('burger');
+        mobileMenu.classList.remove('hide');
+        body.classList.add('noscroll');
+    } else {
+        mobileMenu.classList.add('hide');
+        menuBtns.classList.remove('burger');
+        body.classList.remove('noscroll');
+    }
+})
+
 console.log("Hello, World!");
